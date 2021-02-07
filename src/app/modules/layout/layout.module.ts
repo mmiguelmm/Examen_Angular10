cfddb22ee4b956449ce98611784be70d3c04833a
mmiguelmm <mmirandm@everis.com>
2021-02-07 18:10:49 -0500

    feat(1d):Crear el Layout según las especificaciones del wireframe

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './pages/layout/layout.component';
+
+//Componetes Reutilizable//
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-
+//
 import { CourseListPageComponent} from '../courses/pages/course-list-page/course-list-page.component';
 
 @NgModule({
